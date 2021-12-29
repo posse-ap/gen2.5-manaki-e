1af8e9aaee8ced6931ea8ee7039ed5c9459e9bf1
manaki-e <48690114s@gmail.com>
2021-12-29 22:13:17 +0900

    余分なidを削除

--- quizy.js
+++ quizy.js
@@ -17,37 +17,36 @@ function shuffleArray(inputArray){
 
 for (let i = 0; i < answers.length; i++) {
   shuffleArray(option[i]);
-  document.body.insertAdjacentHTML('beforeend', '<div class="main" id="main"></div>');
+
+  document.body.insertAdjacentHTML('beforeend', '<div class="main"></div>');
   const main = document.getElementsByClassName("main")[i];
   main.insertAdjacentHTML('beforeend', `<h1 class="question">${i + 1}. この地名はなんて読む？</h1>`);
   main.insertAdjacentHTML('beforeend', `<div class="picture"><img src="picture/question${i}.png" alt=""></div>`);
-  main.insertAdjacentHTML('beforeend', '<div class="option-box" id="option-box"></div>');
+  main.insertAdjacentHTML('beforeend', '<div class="option-box"></div>');
   const optionBox = document.getElementsByClassName("option-box")[i];
 
   for (let j = 0; j < 3; j++) {
     optionBox.insertAdjacentHTML('beforeend', `<h3 class="option" id="${i}_${j}" onclick="addClass('${i}', '${j}')">` + option[i][j] + "</h3>")
   };
 
-  main.insertAdjacentHTML('beforeend', '<div class="answerBox correct" id="sucsess"></div>');
-  const sucsess = document.getElementsByClassName('correct')[i];
-  sucsess.insertAdjacentHTML('beforeend', '<p class="correctResult">正解！</p>');
-  sucsess.insertAdjacentHTML('beforeend', `<p class="answerSentence">正解は「${answers[i]}」です！</p>`);
-  main.insertAdjacentHTML('beforeend', '<div class="answerBox wrong" id="failed"></div>');
-  const failed = document.getElementsByClassName('wrong')[i];
-  failed.insertAdjacentHTML('beforeend', '<p class="wrongResult">不正解！</p>');
-  failed.insertAdjacentHTML('beforeend', `<p class="answerSentence">正解は「${answers[i]}」です！</p>`);
+  main.insertAdjacentHTML('beforeend', '<div class="answerBox correct"></div>');
+  const correct = document.getElementsByClassName('correct')[i];
+  correct.insertAdjacentHTML('beforeend', '<p class="correctResult">正解！</p>');
+  correct.insertAdjacentHTML('beforeend', `<p class="answerSentence">正解は「${answers[i]}」です！</p>`);
+  main.insertAdjacentHTML('beforeend', '<div class="answerBox wrong"></div>');
+  const wrong = document.getElementsByClassName('wrong')[i];
+  wrong.insertAdjacentHTML('beforeend', '<p class="wrongResult">不正解！</p>');
+  wrong.insertAdjacentHTML('beforeend', `<p class="answerSentence">正解は「${answers[i]}」です！</p>`);
 }
 
-function addClass(questionId, clickedId) {
-  let aaaaa = option[questionId].indexOf(answers[questionId]);
-  document.getElementById(questionId + "_" + aaaaa).classList.add('true');
-  console.log(aaaaa);
-  // document.getElementsByClassName().classList.add('true');
-  document.getElementsByClassName('option-box')[questionId].classList.add('clicked-option-box');
-  if (option[questionId][clickedId] === answers[questionId]) {
-    document.getElementsByClassName("answerBox")[2 * questionId].classList.remove("correct");
+function addClass(options, selected) {
+  let select = option[options].indexOf(answers[options]);
+  document.getElementById(options + "_" + select).classList.add('true');
+  document.getElementsByClassName('option-box')[options].classList.add('clicked-option-box');
+  if (option[options][selected] === answers[options]) {
+    document.getElementsByClassName("answerBox")[2 * options].classList.remove("correct");
   } else {
-    document.getElementById(questionId + "_" + clickedId).classList.add("false");
-    document.getElementsByClassName("answerBox")[2 * questionId + 1].classList.remove("wrong");
+    document.getElementById(options + "_" + selected).classList.add("false");
+    document.getElementsByClassName("answerBox")[2 * options + 1].classList.remove("wrong");
   }
 }
