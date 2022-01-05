85d85df84b671e33625809f943786a3085e38e4f
manaki-e <48690114s@gmail.com>
2022-01-05 13:34:08 +0900

    全体的に見やすいように変更

--- quizy.js
+++ quizy.js
@@ -22,39 +22,40 @@ function shuffleArray(inputArray) { //選択肢をランダムに表示
 for (let i = 0; i < answers.length; i++) { //表示させたいHTMLの繰り返し
   shuffleArray(option[i]);
 
-  document.body.insertAdjacentHTML('beforeend', '<div class="main"></div>'); //設問を囲むdivの表示
-  const main = document.getElementsByClassName("main")[i];
-  var html = `<h1 class="question">${i + 1}. この地名はなんて読む？</h1>` //問題文の表示
-  html += `<div class="picture"><img src="picture/question${i}.png" alt=""></div>`; //写真の表示
-  html += '<div class="optionBox"></div>'; //選択肢を囲むdivの表示
-  main.insertAdjacentHTML('beforeend', html);
-  
+  let html =
+    '<div class="main">' + //設問を囲むdivの表示
+    `<h1 class="question">${i + 1}. この地名はなんて読む？</h1>` + //問題文の表示
+    `<div class="picture"><img src="picture/question${i}.png" alt=""></div>` + //写真の表示
+    '<div class="optionBox">'; //選択肢を囲むdivの表示
+
   for (let j = 0; j < option[i].length; j++) { //選択肢の表示
-    const optionBox = document.getElementsByClassName("optionBox")[i];
-    optionBox.insertAdjacentHTML('beforeend', `<h3 class="option" id="${i}_${j}" onclick="addClass('${i}', '${j}')">` + option[i][j] + "</h3>")
+    html += `<h3 class="option" id="${i}_${j}" onclick="addClass('${i}', '${j}')">` + option[i][j] + "</h3>"
   };
 
-  main.insertAdjacentHTML('beforeend', '<div class="answerBox correct"></div>'); //正解回答ボックスの準備
-  const correct = document.getElementsByClassName('correct')[i];
-  correct.insertAdjacentHTML('beforeend', '<p class="correctResult">正解！</p>');
-  correct.insertAdjacentHTML('beforeend', `<p class="answerSentence">正解は「${answers[i]}」です！</p>`);
+  html +=
+    '</div>' +
+    '<div class="answerBox correct">' + //正解回答ボックスの準備
+    '<p class="correctResult">正解！</p>' +
+    `<p class="answerSentence">正解は「${answers[i]}」です！</p>` +
+    '</div>' +
+    '<div class="answerBox wrong">' + //不正解回答ボックスの準備
+    '<p class="wrongResult">不正解！</p>' +
+    `<p class="answerSentence">正解は「${answers[i]}」です！</p>` +
+    '</div></div>';
 
-  main.insertAdjacentHTML('beforeend', '<div class="answerBox wrong"></div>'); //不正解回答ボックスの準備
-  const wrong = document.getElementsByClassName('wrong')[i];
-  wrong.insertAdjacentHTML('beforeend', '<p class="wrongResult">不正解！</p>');
-  wrong.insertAdjacentHTML('beforeend', `<p class="answerSentence">正解は「${answers[i]}」です！</p>`);
+  document.currentScript.insertAdjacentHTML('beforebegin', html);
 }
 
 function addClass(options, selected) { //選択肢がクリックされたときの挙動
 
   let select = option[options].indexOf(answers[options]);
-  document.getElementById(options + "_" + select).classList.add('true'); //正解の選択肢を赤くする
+  document.getElementById(options + "_" + select).classList.add('true'); //正解の選択肢を青くする
   document.getElementsByClassName('optionBox')[options].classList.add('clickedOptionBox'); // 二度クリックさせない
 
-  if (option[options][selected] === answers[options]) { //クリックされた選択肢と正解が一致していた時
+  if (option[options][selected] === answers[options]) { //クリックされた選択肢と正解が一致
     document.getElementsByClassName("answerBox")[2 * options].classList.remove("correct"); //正解回答ボックスの表示
-  } else {
-    document.getElementById(options + "_" + selected).classList.add("false"); //クリックした選択肢を青くする
+  } else { //クリックされた選択肢と正解が不一致
+    document.getElementById(options + "_" + selected).classList.add("false"); //クリックした選択肢を赤くする
     document.getElementsByClassName("answerBox")[2 * options + 1].classList.remove("wrong"); //不正解回答ボックスの表示
   }
 }
