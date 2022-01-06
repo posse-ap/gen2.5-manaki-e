99e78f5d9e164043eaf8ecef52b25ce13defe219
manaki-e <48690114s@gmail.com>
2022-01-07 01:32:43 +0900

    data

--- quizy.js
+++ quizy.js
@@ -1,73 +1,77 @@
 'use strict'
 
-const answers = ["たかなわ", "かめいど", "こうじまち", "おなりもん", "とどろき", "しゃくじい", "ぞうしき", "おかちまち", "ししぼね", "こぐれ"] //各設問の正解
-
 const option = [ //各設問の正解を含む選択肢
-  [answers[0], "こうわ", "たかわ"],
-  [answers[1], "かめど", "かめと"],
-  [answers[2], "おかとまち", "かゆまち"],
-  [answers[3], "ごせいもん", "おかどもん"],
-  [answers[4], "たたら", "たたりき"],
-  [answers[5], "いじい", "せきこうい"],
-  [answers[6], "ざっしき", "ざっしょく"],
-  [answers[7], "みとちょう", "ごしろちょう"],
-  [answers[8], "しこね", "ろっこつ"],
-  [answers[9], "こばく", "こしゃく"],
+  ["たかなわ", "こうわ", "たかわ"],
+  ["かめいど", "かめど", "かめと"],
+  ["こうじまち", "おかとまち", "かゆまち"],
+  ["おなりもん", "ごせいもん", "おかどもん"],
+  ["とどろき", "たたら", "たたりき"],
+  ["しゃくじい", "いじい", "せきこうい"],
+  ["ぞうしき", "ざっしき", "ざっしょく"],
+  ["おかちまち", "みとちょう", "ごしろちょう"],
+  ["ししぼね", "しこね", "ろっこつ"],
+  ["こぐれ", "こばく", "こしゃく"],
 ]
+
+let answers = new Array();
+option.forEach(element => {
+  answers.push(element[0])
+});
+
+console.log("answers", answers);
 
 function shuffleArray(inputArray) { //選択肢をランダムに表示
   inputArray.sort(() => Math.random() - 0.5);
 }
+
+let answerNumber = null;
 
 for (let i = 0; i < answers.length; i++) { //表示させたいHTMLの繰り返し
   shuffleArray(option[i]);
 
   let html =
     '<div class="main">' + //設問を囲むdivの表示
-    `<h1 class="question">${i + 1}. この地名はなんて読む？</h1>` + //問題文の表示
-    `<div class="picture"><img src="picture/question${i}.png" alt=""></div>` + //写真の表示
-    '<div class="optionBox">'; //選択肢を囲むdivの表示
-
+      `<h1 class="question">${i + 1}. この地名はなんて読む？</h1>` + //問題文の表示
+      '<div class="picture">' +
+        `<img src="picture/question${i}.png" alt="">` + //写真の表示
+      '</div>' +
+      '<ul class="optionBox">'; //選択肢を囲むdivの表示
+  answerNumber = option[i].indexOf(answers[i]);
+  console.log("answer", answerNumber);
   for (let j = 0; j < option[i].length; j++) { //選択肢の表示
-    html += `<h3 class="option" id="${i}_${j}" onclick="selectProcess('${i}', '${j}')">` + option[i][j] + "</h3>"
+    html += 
+        `<li class="option" id="${i}_${j}" onclick="selectProcess('${i}', '${j}')" data-answer-number = "${answerNumber}">` + 
+          option[i][j] + 
+        "</li>";
   };
 
   html +=
-    '</div>' +
-    '<div class="answerBox correct">' + //正解回答ボックスの準備
-    '<p class="correctResult">正解！</p>' +
-    `<p class="answerSentence">正解は「${answers[i]}」です！</p>` +
-    '</div>' +
-    '<div class="answerBox wrong">' + //不正解回答ボックスの準備
-    '<p class="wrongResult">不正解！</p>' +
-    `<p class="answerSentence">正解は「${answers[i]}」です！</p>` +
-    '</div></div>';
+      '</ul>' +
+      '<div class="answerBox correct">' + //正解回答ボックスの準備
+        '<p class="correctResult">正解！</p>' +
+        `<p class="answerSentence">正解は「${answers[i]}」です！</p>` +
+      '</div>' +
+      '<div class="answerBox wrong">' + //不正解回答ボックスの準備
+        '<p class="wrongResult">不正解！</p>' +
+        `<p class="answerSentence">正解は「${answers[i]}」です！</p>` +
+      '</div>' +
+    '</div>';
 
   document.currentScript.insertAdjacentHTML('beforebegin', html);
 }
 
 function selectProcess(options, selected) { //選択肢がクリックされたときの挙動
 
-  let answerNumber= findAnswerNumber(options);
+ //データ属性からanswerNumberを取得
+
+  let answerNumber= document.getElementById(options + "_" + selected).dataset.answerNumber;
   document.getElementById(options + "_" + answerNumber).classList.add('true'); //正解の選択肢を青くする
   document.getElementsByClassName('optionBox')[options].classList.add('clickedOptionBox'); // 二度クリックさせない
 
-  if (judge(options, selected)) { //クリックされた選択肢と正解が一致
+  if (answerNumber == selected) { //クリックされた選択肢と正解が一致
     document.getElementsByClassName("answerBox")[2 * options].classList.remove("correct"); //正解回答ボックスの表示
   } else { //クリックされた選択肢と正解が不一致
     document.getElementById(options + "_" + selected).classList.add("false"); //クリックした選択肢を赤くする
     document.getElementsByClassName("answerBox")[2 * options + 1].classList.remove("wrong"); //不正解回答ボックスの表示
   }
-}
-
-/* ################################
-関数の勉強として切り出しました
-###################################*/
-
-function findAnswerNumber(options) {
-  return option[options].indexOf(answers[options])
-}
-
-function judge(options, selected) {
-  return option[options][selected] === answers[options]
 }
