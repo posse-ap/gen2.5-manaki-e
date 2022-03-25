048b7e517aa8892cbeef5524a8b38c0a68054378
manaki-e <48690114s@gmail.com>
2022-03-25 19:21:08 +0900

    スタイルを修正

--- js/modal.js
+++ js/modal.js
@@ -14,21 +14,21 @@ $(function () {
     $('.modals').fadeOut();
     $('html').removeClass('modalset');
   });
-  
+
   $('.modal__inner__push button').click(function () {
     $('.modal__inner1').fadeOut();
-    setTimeout(function() {
+    setTimeout(function () {
       $('.modal__inner2').fadeIn();
     }, 500);
-    setTimeout(function() {
+    setTimeout(function () {
       $('.modal__inner2').fadeOut();
-      setTimeout(function() {
+      setTimeout(function () {
         $('.modal__inner3').fadeIn();
-        $(document).click(function() {
+        $(document).click(function () {
           location.reload();
         })
       }, 500)
     }, 3000);
-    
+
   });
 });	
