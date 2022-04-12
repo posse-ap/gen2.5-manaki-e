c0cdf128e9c95a109b3a3c71d29a254c56db308c
manaki-e <48690114s@gmail.com>
2022-04-12 13:46:46 +0900

    twitter設定 フォーマットの修正

--- js/modal.js
+++ js/modal.js
@@ -1,34 +1,46 @@
 $(function () {
-
-  $('.header__submit button').click(function () {
-    $('.modals').fadeIn();
-    $('html').addClass('modalset');
+  $(".header__submit button").click(function () {
+    $(".modals").fadeIn();
+    $("html").addClass("modalset");
   });
 
-  $('.sp__push').click(function () {
-    $('.modals').fadeIn();
-    $('html').addClass('modalset');
+  $(".sp__push").click(function () {
+    $(".modals").fadeIn();
+    $("html").addClass("modalset");
   });
 
-  $('.modal__inner__close').click(function () {
-    $('.modals').fadeOut();
-    $('html').removeClass('modalset');
+  $(".modal__inner__close").click(function () {
+    $(".modals").fadeOut();
+    $("html").removeClass("modalset");
   });
 
-  $('.modal__inner__push button').click(function () {
-    $('.modal__inner1').fadeOut();
+  $(".modal__inner__push button").click(function () {
+    $(".modal__inner1").fadeOut();
     setTimeout(function () {
-      $('.modal__inner2').fadeIn();
+      $(".modal__inner2").fadeIn();
     }, 500);
     setTimeout(function () {
-      $('.modal__inner2').fadeOut();
+      $(".modal__inner2").fadeOut();
       setTimeout(function () {
-        $('.modal__inner3').fadeIn();
+        $(".modal__inner3").fadeIn();
         $(document).click(function () {
           location.reload();
-        })
-      }, 500)
+        });
+      }, 500);
     }, 3000);
-
   });
-});	
+});
+
+let twitter = document.getElementById("twitter");
+
+$(function () {
+  $(".push__button").click(function () {
+    if (twitter.checked) {
+      let twitter__text = document.getElementById("twitter__text");
+      $("a").attr(
+        "href",
+        `https://twitter.com/intent/tweet?text=${twitter__text.value}`
+      );
+    }
+  });
+});
--- js/pie__chart.js
+++ js/pie__chart.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 let month = document.getElementById("monthly__statistics");
 let languages = document.getElementById("languages__statistics");
@@ -14,7 +14,6 @@ var plugin1 = {
     var ctx = chart.ctx;
 
     chart.data.datasets.forEach(function (dataset, i) {
-
       var dataSum = 0;
       dataset.data.forEach(function (element) {
         dataSum += element;
@@ -24,85 +23,89 @@ var plugin1 = {
       if (!meta.hidden) {
         meta.data.forEach(function (element, index) {
           // Draw the text in black, with the specified font
-          ctx.fillStyle = 'rgb(255, 255, 255)';
+          ctx.fillStyle = "rgb(255, 255, 255)";
 
           var fontSize = 1;
           ctx.font = Chart.helpers.fontString(fontSize);
 
-
           if (dataset.data[index] > 10) {
-
             // Just naively convert to string for now
-            var dataString = (Math.round(dataset.data[index] / dataSum * 1000) / 10).toString() + "%";
+            var dataString =
+              (
+                Math.round((dataset.data[index] / dataSum) * 1000) / 10
+              ).toString() + "%";
 
             // Make sure alignment settings are correct
-            ctx.textAlign = 'center';
-            ctx.textBaseline = 'middle';
+            ctx.textAlign = "center";
+            ctx.textBaseline = "middle";
 
             var padding = 5;
             var position = element.tooltipPosition();
-            ctx.fillText(dataString, position.x, position.y + (fontSize / 2) - padding);
+            ctx.fillText(
+              dataString,
+              position.x,
+              position.y + fontSize / 2 - padding
+            );
           }
         });
       }
     });
-  }
+  },
 };
-
 
 let chart1 = new Chart(languages, {
   type: "doughnut",
   data: {
     // labels: ["JavaScript", "CSS", "PHP", "HTML", "Lalavel", "SQL", "SHELL", "情報システム基礎知識（その他）"],
-    datasets: [{
-      backgroundColor: [
-        "blue",
-        "rgb(91, 64, 243)",
-        "rgb(64, 147, 243)",
-        "rgb(135, 210, 245)",
-        "rgb(231, 175, 236)",
-        "rgb(97, 8, 105)",
-        "rgb(72, 29, 112)",
-        "rgb(113, 115, 235)"
-      ],
-      data: [5.9, 11.8, 23.5, 14.7, 8.8, 29.4, 5.9, 0],
-      borderWidth: [1]
-    }]
+    datasets: [
+      {
+        backgroundColor: [
+          "blue",
+          "rgb(91, 64, 243)",
+          "rgb(64, 147, 243)",
+          "rgb(135, 210, 245)",
+          "rgb(231, 175, 236)",
+          "rgb(97, 8, 105)",
+          "rgb(72, 29, 112)",
+          "rgb(113, 115, 235)",
+        ],
+        data: [5.9, 11.8, 23.5, 14.7, 8.8, 29.4, 5.9, 0],
+        borderWidth: [1],
+      },
+    ],
   },
   options: {
     title: {
       display: false,
-      text: '学習言語'
+      text: "学習言語",
     },
     tooltips: {
-      enabled: false
+      enabled: false,
     },
   },
-  plugins: [plugin1]
+  plugins: [plugin1],
 });
 
 let chart2 = new Chart(contents, {
   type: "doughnut",
   data: {
     // labels: ["ドットインストール", "N予備校", "POSSE課題"],
-    datasets: [{
-      backgroundColor: [
-        "blue",
-        "rgb(91, 64, 243)",
-        "rgb(64, 147, 243)",
-      ],
-      data: [94.1, 0, 5.9],
-      borderWidth: [1]
-    }]
+    datasets: [
+      {
+        backgroundColor: ["blue", "rgb(91, 64, 243)", "rgb(64, 147, 243)"],
+        data: [94.1, 0, 5.9],
+        borderWidth: [1],
+      },
+    ],
   },
   options: {
     title: {
       display: false,
-      text: '学習コンテンツ'
+      text: "学習コンテンツ",
     },
     tooltips: {
-      enabled: false
+      enabled: false,
     },
   },
-  plugins: [plugin1]
+  plugins: [plugin1],
 });
