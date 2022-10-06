66bcf9dd45a939eda81996ba9d31974365cb8b2d
manaki-e <48690114s@gmail.com>
2022-10-06 10:32:44 +0900

    変数のtypo修正

--- src/api/getContentInfo.php
+++ src/api/getContentInfo.php
@@ -3,18 +3,18 @@ require('../dbconnect.php');
 header('Content-Type: application/json; charset=UTF-8');
 
 try {
-  $stmt_hourLanguage = $db->prepare('SELECT contents.content, sum(hour) AS hour FROM post LEFT JOIN contents ON post.content_id = contents.id GROUP BY content_id ORDER BY content_id');
-  $stmt_hourLanguage->execute();
-  $hour_language = $stmt_hourLanguage->fetchAll(PDO::FETCH_KEY_PAIR);
+  $stmt_hourContent = $db->prepare('SELECT contents.content, sum(hour) AS hour FROM post LEFT JOIN contents ON post.content_id = contents.id GROUP BY content_id ORDER BY content_id');
+  $stmt_hourContent->execute();
+  $hour_content = $stmt_hourContent->fetchAll(PDO::FETCH_KEY_PAIR);
 
-  $hour_language_set = [];
+  $hour_content_set = [];
 
-  foreach ($hour_language as $key => $value) {
-    $hour_language_set[$key] = $value;
-    unset($hour_language[$key]);
+  foreach ($hour_content as $key => $value) {
+    $hour_content_set[$key] = $value;
+    unset($hour_content[$key]);
   }
 
-  echo json_encode($hour_language_set, JSON_UNESCAPED_UNICODE);
+  echo json_encode($hour_content_set, JSON_UNESCAPED_UNICODE);
 } catch (PDOException $e) {
   echo $e->getMessage();
   exit();
